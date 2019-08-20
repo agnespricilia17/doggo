392c5e544f8318c83fc8a3fa0032b078e8b61711
Agnes Pricilia <agnespricilia17@gmail.com>
2019-08-20 08:43:58 +0700

    Initial Commit

---
+++ doggo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  doggo
+//
+//  Created by agnes pricilia on 20/08/19.
+//  Copyright © 2019 agnes pricilia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
