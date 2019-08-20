9d9ebfae4791d0791a9497d658b026ba6198686d
Agnes Pricilia <agnespricilia17@gmail.com>
2019-08-20 14:19:38 +0700

    Added hierarchy Added necessary storyboard Connected controller to each storyboard

---
+++ doggo/Controller/WeightViewController.swift
@@ -0,0 +1,30 @@
+//
+//  WeightViewController.swift
+//  doggo
+//
+//  Created by agnes pricilia on 20/08/19.
+//  Copyright © 2019 agnes pricilia. All rights reserved.
+//
+
+import UIKit
+
+class WeightViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
