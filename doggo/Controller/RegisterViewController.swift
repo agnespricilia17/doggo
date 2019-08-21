a373e722d533a2d6767c550c2a509bb413b59690
Agnes Pricilia <agnespricilia17@gmail.com>
2019-08-21 14:34:38 +0700

    Create interface for Choose Breed Page and Register Page

--- doggo/Controller/RegisterViewController.swift
+++ doggo/Controller/RegisterViewController.swift
@@ -10,13 +10,45 @@ import UIKit
 
 class RegisterViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var yellowCircle: UIView!
+    
+    @IBOutlet weak var breedLabel: UILabel!
+    
+    @IBOutlet weak var registerDone: UIButton!
+    @IBOutlet weak var nameField: UITextField!
+    @IBAction func registerButton(_ sender: UIButton) {
+        let alert = UIAlertController(title: "Confirmation", message: "Do you want to save this data? Please make sure the data is correct.", preferredStyle: .alert)
+        
+        alert.addAction(UIAlertAction(title: "Register"
+            , style: .default
+            , handler: { (UIAlertAction) in
+                self.confirmRegistration()
+        }))
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        
+        self.present(alert, animated: true)
     }
     
-
+   
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        yellowCircle.layer.cornerRadius = yellowCircle.frame.width/2
+        // Do any additional setup after loading the view.
+        
+        registerDone.layer.cornerRadius = registerDone.frame.size.height/2
+        registerDone.clipsToBounds = true
+        registerDone.layer.borderWidth = 1
+        registerDone.layer.borderColor = UIColor.blue.cgColor
+       
+        
+    }
+    func confirmRegistration() {
+        performSegue(withIdentifier: "goToDashboard", sender: self)
+        
+    }
+    
+ 
     /*
     // MARK: - Navigation
 
