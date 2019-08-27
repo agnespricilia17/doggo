b8ced604651a153f0d51bae75ee1a6f42ec96326
SetyaG <owen.pg27@gmail.com>
2019-08-27 11:57:09 +0700

    Fixed collection view into scroll view, and fixed placing of the label (still not dynamic)

--- doggo/Controller/DashboardViewController.swift
+++ doggo/Controller/DashboardViewController.swift
@@ -8,26 +8,80 @@
 
 import UIKit
 
-class DashboardViewController: UIViewController {
+class DashboardViewController: UIViewController, UIScrollViewDelegate {
     
     // Element
     @IBOutlet weak var yellowCircle: UIView!
-
-    @IBOutlet weak var dogListControl: UIPageControl!
     
     // Data and Texts
     @IBOutlet weak var dogName: UILabel!
     @IBOutlet weak var dogBreed: UILabel!
     
+    var slides:[PhotoSlide] = [];
+    
+    @IBOutlet weak var dogScrollView: UIScrollView!
+    @IBOutlet weak var dogPageControl: UIPageControl!
     @IBOutlet weak var bubbleMessage: UILabel!
     @IBOutlet weak var bubbleView: UIView!
+    
+    @IBOutlet weak var foodButton: UIButton!
+    @IBOutlet weak var groomingButton: UIButton!
+    @IBOutlet weak var checkUpButton: UIButton!
+    
+    // Labels and Icons
+    @IBOutlet weak var scheduleLabel: UILabel!
+    @IBOutlet weak var foodScheduleMainLabel: UILabel!
+    @IBOutlet weak var foodScheduleSecondaryLabel: UILabel!
+    @IBOutlet weak var groomingScheduleMainLabel: UILabel!
+    @IBOutlet weak var groomingScheduleSecondaryLabel: UILabel!
+    @IBOutlet weak var checkUpMainLabel: UILabel!
+    @IBOutlet weak var checkUpSecondaryLabel: UILabel!
+    
+    @IBOutlet weak var foodIcon: UIImageView!
+    @IBOutlet weak var foodIconLabel: UILabel!
+    @IBOutlet weak var groomingIcon: UIImageView!
+    @IBOutlet weak var groomingIconLabel: UILabel!
+    @IBOutlet weak var checkUpIcon: UIImageView!
+    @IBOutlet weak var checkUpIconLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         yellowCircle.layer.cornerRadius = yellowCircle.frame.width / 2
         
+        // Setting up dog's name and breed
         setDogName()
+        
+        // Setting up scroll view for dog lists
+        dogScrollView.delegate = self
+        slides = createSlides()
+        setupSlideScrollView(slides: slides)
+        
+        dogPageControl.numberOfPages = slides.count
+        dogPageControl.currentPage = 0
+        view.bringSubviewToFront(dogPageControl)
+        
+        view.bringSubviewToFront(dogName)
+        view.bringSubviewToFront(dogBreed)
+        view.bringSubviewToFront(yellowCircle)
+        view.bringSubviewToFront(bubbleMessage)
+        view.bringSubviewToFront(bubbleView)
+        view.bringSubviewToFront(scheduleLabel)
+        view.bringSubviewToFront(foodButton)
+        view.bringSubviewToFront(groomingButton)
+        view.bringSubviewToFront(checkUpButton)
+        view.bringSubviewToFront(foodScheduleMainLabel)
+        view.bringSubviewToFront(foodScheduleSecondaryLabel)
+        view.bringSubviewToFront(foodIcon)
+        view.bringSubviewToFront(foodIconLabel)
+        view.bringSubviewToFront(groomingScheduleMainLabel)
+        view.bringSubviewToFront(groomingScheduleSecondaryLabel)
+        view.bringSubviewToFront(groomingIcon)
+        view.bringSubviewToFront(groomingIconLabel)
+        view.bringSubviewToFront(checkUpMainLabel)
+        view.bringSubviewToFront(checkUpSecondaryLabel)
+        view.bringSubviewToFront(checkUpIcon)
+        view.bringSubviewToFront(checkUpIconLabel)
         
         // Set the text to be displayed in bubble here
         createBubbleShape(view: bubbleView)
@@ -46,16 +100,60 @@ class DashboardViewController: UIViewController {
     }
     
     @IBAction func foodPageButton(_ sender: Any) {
-        
+        // Go to Food page
     }
     
     @IBAction func groomPageButton(_ sender: Any) {
-        
+        // Go to Grooming page
     }
     
     @IBAction func checkUpPageButton(_ sender: Any) {
-        
+        // Go to Check Up page
     }
+    
+////////////////////////////////////////////////////////////////////
+    
+    func createSlides() -> [PhotoSlide] {
+        
+        let slide1:PhotoSlide = Bundle.main.loadNibNamed("PhotoSlide", owner: self, options: nil)?.first as! PhotoSlide
+        slide1.dogImage.image = UIImage(named: "labrador_retriever_PNG81")
+        
+        let slide2:PhotoSlide = Bundle.main.loadNibNamed("PhotoSlide", owner: self, options: nil)?.first as! PhotoSlide
+        slide2.dogImage.image = UIImage(named: "labrador_retriever_PNG81")
+        
+        let slide3:PhotoSlide = Bundle.main.loadNibNamed("PhotoSlide", owner: self, options: nil)?.first as! PhotoSlide
+        slide3.dogImage.image = UIImage(named: "labrador_retriever_PNG81")
+        
+        return [slide1, slide2, slide3]
+    }
+    
+    func setupSlideScrollView(slides : [PhotoSlide]) {
+        dogScrollView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height)
+        dogScrollView.contentSize = CGSize(width: view.frame.width * CGFloat(slides.count), height: view.frame.height)
+        dogScrollView.isPagingEnabled = true
+        
+        for i in 0 ..< slides.count {
+            slides[i].frame = CGRect(x: view.frame.width * CGFloat(i), y: 0, width: view.frame.width, height: view.frame.height)
+            dogScrollView.addSubview(slides[i])
+        }
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let pageIndex = round(scrollView.contentOffset.x/view.frame.width)
+        dogPageControl.currentPage = Int(pageIndex)
+        
+        let maximumHorizontalOffset: CGFloat = scrollView.contentSize.width - scrollView.frame.width
+        let currentHorizontalOffset: CGFloat = scrollView.contentOffset.x
+        
+        // vertical
+        let maximumVerticalOffset: CGFloat = scrollView.contentSize.height - scrollView.frame.height
+        let currentVerticalOffset: CGFloat = scrollView.contentOffset.y
+        
+        let percentageHorizontalOffset: CGFloat = currentHorizontalOffset / maximumHorizontalOffset
+        let percentageVerticalOffset: CGFloat = currentVerticalOffset / maximumVerticalOffset
+    }
+    
+//////////////////////////////////////////////////////////////////////
     
     
     func createBubbleShape(view: UIView) {
@@ -85,7 +183,7 @@ class DashboardViewController: UIViewController {
         bezierPath.fill()
         
         bubbleLayer.strokeColor = UIColor.red.cgColor
-        bubbleLayer.lineWidth = 1
+        bubbleLayer.lineWidth = 3
         bubbleLayer.fillColor = UIColor.clear.cgColor
         bubbleLayer.path = bezierPath.cgPath
         
@@ -95,9 +193,9 @@ class DashboardViewController: UIViewController {
     
     // Create bubble message; *set for daily random message*
     func produceBubbleMessage(text: String) {
-//        bubbleMessage.numberOfLines = 0
-//        bubbleMessage.font = UIFont.systemFont(ofSize: 14)
-//        bubbleMessage.textColor = .black
+        //        bubbleMessage.numberOfLines = 0
+        //        bubbleMessage.font = UIFont.systemFont(ofSize: 14)
+        //        bubbleMessage.textColor = .black
         bubbleMessage.text = text
         
         let constraintRect = CGSize(width: 0.66 * view.frame.width,
